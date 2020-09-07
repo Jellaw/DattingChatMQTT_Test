d208764fa0a5cdda32b3c6688e46f3331309ed6f
Jellaw <123duy99@gmail.com>
2020-09-07 15:33:47 +0700

    2.0

--- app/src/main/java/com/example/datting/MainActivity.java
+++ app/src/main/java/com/example/datting/MainActivity.java
@@ -20,9 +20,11 @@ public class MainActivity extends AppCompatActivity {
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, TestMQTT.class);
+
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
                 startActivity(intent);
                 finish();
+
             }
         });
     }
